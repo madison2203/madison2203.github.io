f3ea482e18f13d91227599b87284100153d1c03e
madison2203 <s4026051@rmit.edu.vn>
2024-10-27 00:10:14 +1100

    asm3 update

--- asm3/script.js
+++ asm3/script.js
@@ -1,6 +1,9 @@
-document.querySelectorAll(".img").forEach((b1) => {
-  b1.onclick = () => {
+document.querySelectorAll(".image-container img").forEach((saigoncityhall) => {
+  saigoncityhall.onclick = () => {
     document.querySelector(".popup-image").style.display = "block";
     document.querySelector(".popup-image img").src = image.getAttribute("src");
   };
 });
+document.querySelector(".popup-image span").onclick = () => {
+  document.querySelector(".popup-image").style.display = "none";
+};
