89223b36e6b51600eda22cd367b45068d463dcbb
madison2203 <s4026051@rmit.edu.vn>
2024-10-28 04:21:22 +1100

    asm3

--- asm3/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".image-container img").forEach((saigoncityhall) => {
-  saigoncityhall.onclick = () => {
-    document.querySelector(".popup-image").style.display = "block";
-    document.querySelector(".popup-image img").src = image.getAttribute("src");
-  };
-});
-document.querySelector(".popup-image span").onclick = () => {
-  document.querySelector(".popup-image").style.display = "none";
-};
